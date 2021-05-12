d564fedd12505c7137686a2bbda506317e42fd58
Bhargava Chary Peddapudi <pedapudibhargav@gmail.com>
2021-05-12 16:52:56 -0700

    First push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import PageEditor from './containers/page-editor/PageEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <PageEditor></PageEditor>
+    </div>
+  );
+}
+
+export default App;
