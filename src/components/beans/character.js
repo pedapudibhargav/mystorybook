4a13b7e061a2912e90b6e4c41567d8989166b68c
Bhargava Chary Peddapudi <pedapudibhargav@gmail.com>
2021-05-26 18:40:17 -0700

    Open creation flow updated

---
+++ src/components/beans/character.js
@@ -0,0 +1,7 @@
+class Character{
+    constructor(name,order){
+        this.name = name;
+        this.order = order;
+    }
+}
+export default Character;
--- src/components/pages/open-creation-flow/OpenCreationFlow.js
+++ src/components/pages/open-creation-flow/OpenCreationFlow.js
@@ -1,100 +1,176 @@
-import React, { useState } from "react";
-import './OpenCreationFlow.scss'
-import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { Box, CssBaseline, MuiThemeProvider, ThemeProvider, Typography, useMediaQuery } from "@material-ui/core";
+import React from "react";
+import Character from "../../beans/character";
 
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(0),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-}));
+class OpenCreationFlow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfAdults: 0,
+            numOfKids: 1,
+            adults: [],
+            kids: [],
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        //condition to check if it's adult character name
+        if (target.classList.contains('character-name')) {
+
+            // if adult character get index and name of the adult and save it to the adults array
+            let charorder = target.getAttribute('charorder');
+            let tmpArray = target.getAttribute('isadult') == 'true' ? this.state.adults : this.state.kids;
+            let tmpCharacter = new Character(value, charorder);
+            tmpArray[charorder] = tmpCharacter;
+            if (target.getAttribute('isadult') == 'true') {
+                this.setState({
+                    adults: tmpArray
+                })
+            }
+            else if (target.getAttribute('isadult') == 'false') {
+                this.setState({
+                    kids: tmpArray
+                })
+            }
+            else {
+                console.error("invalid character type");
+            }
+
+        }
+        // fallback condition to regular input fields
+        else {
+            this.setState({
+                [name]: value
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                                <li className="breadcrumb-item active" aria-current="page">Library</li>
+                            </ol>
+                        </nav>
+
+                        <form>
+                            <div className="row">
 
 
-export default function OpenCreationFlow(props) {
-    const classes = useStyles();
-    // const [noOfAdults, setNoOfAdults] = useState(0);
-    const [noOfAdults, setNoOfAdults] = React.useState('');
-    const [noOfKids, setNoOfKids] = React.useState('');
+                                {/* START: get number of adults and kids in the story */}
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="exampleFormControlSelect1">Number of Adults in your story</label>
+                                        <select className="form-control" name="numOfAdults" value={this.state.numOfAdults} onChange={this.handleInputChange}>
+                                            <option>0</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="exampleFormControlSelect2">Number of Kids in your story</label>
+                                        <select className="form-control" name="numOfKids" value={this.state.numOfKids} onChange={this.handleInputChange}>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
 
-    const handleChange = (event) => {
-        setNoOfAdults(event.target.value);
-    };
 
-    const handleKidsNoChange = (event) => {
-        setNoOfKids(event.target.value);
-    };
+                            {/* START: Column for Input fields for adults names */}
+                            <div className="row">
+                                <div className="col">
+                                    {
+                                        this.state.numOfAdults > 0 ? <div className="form-group">
+                                            <label for="exampleFormControlInput1">Name of the adults</label>
+                                            <input
+                                                type="text"
+                                                className="form-control character-name"
+                                                charorder={0}
+                                                isadult="true"
+                                                placeholder={this.state.numOfAdults > 1 ? 'First Adult Name' : 'Enter Name'}
+                                                onChange={this.handleInputChange}
+                                            />
+                                        </div> : null
+                                    }
+                                    {
+                                        this.state.numOfAdults > 1 ? <div className="form-group">
+                                            <input
+                                                type="text"
+                                                className="form-control character-name"
+                                                charorder={1}
+                                                isadult="true"
+                                                placeholder='Second Adult Name'
+                                                onChange={this.handleInputChange}
+                                            />
+                                        </div> : null
+                                    }
+                                </div>
 
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+                                {/* START: Column for Input fields for kids names */}
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="exampleFormControlInput1">Name of the Kids</label>
+                                        <input
+                                            type="text"
+                                            className="form-control character-name"
+                                            charorder={0}
+                                            isadult="false"
+                                            placeholder={this.state.numOfKids > 1 ? 'First Kid\'s Name' : 'Enter the kid\'s Name'}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                    {
+                                        this.state.numOfKids > 1 ?
+                                            <div className="form-group">
+                                                <input
+                                                    type="text"
+                                                    className="form-control character-name"
+                                                    charorder={1}
+                                                    isadult="false"
+                                                    placeholder={'Enter the second kid\'s Name'}
+                                                    onChange={this.handleInputChange}
+                                                />
+                                            </div> : null
+                                    }
+                                    {
+                                        this.state.numOfKids > 2 ?
+                                            <div className="form-group">
+                                                <input
+                                                    type="text"
+                                                    className="form-control character-name"
+                                                    charorder={2}
+                                                    isadult="false"
+                                                    placeholder={'Enter the third kid\'s Name'}
+                                                    onChange={this.handleInputChange}
+                                                />
+                                            </div> : null
+                                    }
+                                </div>
+                            </div>
+                        </form>
 
-    const darkTheme = createMuiTheme({
-        palette: {
-            type: 'dark',
-        },
-    });
-
-    const lightTheme = React.useMemo(
-        () =>
-            createMuiTheme({
-                palette: {
-                    type: 'light',
-                },
-                spacing: 8,
-            }),
-    );
-    return (
-        <div className="open-creation-flow">
-            <MuiThemeProvider theme={darkTheme}>
-                <CssBaseline>
-                    <div className="drawer">
-                        <Box mb={2}>
-                            <Typography variant="h5" gutterBottom align="center" className="ocf-drawer-h" >Pick your characters</Typography>
-                        </Box>
-                        <FormControl className={classes.formControl + ' w-100per m-0'}>
-                            <InputLabel id="no-of-adults-label">Number of Adults</InputLabel>
-                            <Select
-                                labelId="no-of-adults-labe"
-                                value={noOfAdults}
-                                onChange={handleChange}
-                                fullWidth
-                            >
-                                <MenuItem value={0}>0</MenuItem>
-                                <MenuItem value={1}>1</MenuItem>
-                                <MenuItem value={2}>2</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl className={classes.formControl + ' w-100per m-0'}>
-                            <InputLabel id="no-of-kids-label">Number of Kids</InputLabel>
-                            <Select
-                                labelId="no-of-kids-label"
-                                value={noOfKids}
-                                onChange={handleKidsNoChange}
-                                fullWidth
-                            >
-                                <MenuItem value={1}>1</MenuItem>
-                                <MenuItem value={2}>2</MenuItem>
-                                <MenuItem value={2}>3</MenuItem>
-                            </Select>
-                        </FormControl>
+                        <div>
+                            Testing:<br />
+                            {JSON.stringify(this.state)}
+                        </div>
                     </div>
-                </CssBaseline>
-            </MuiThemeProvider>
-            <ThemeProvider theme={lightTheme}>
-                <CssBaseline>
-                    <div className="content">
-                        <p>{noOfAdults}</p>
-                        <p>{noOfKids}</p>
-                    </div>
-                </CssBaseline>
-            </ThemeProvider>
-        </div>
-    )
+                </div>
+            </div>
+        );
+    }
 }
+export default OpenCreationFlow;
